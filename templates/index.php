26c8f42252de4309260457848c1e4ad1a9cc1b41
atkozie <atkozie@gmail.com>
2016-05-04 00:10:29 -0700

    wordpress functions
    added basic functions to complete template

--- templates/index.php
+++ templates/index.php
@@ -1,12 +1,19 @@
 <!doctype html>
 <html>
+    <?php wp_head(); ?>
 <head>
 <meta charset="UTF-8">
-<title>Adam Kozie : Web Design</title>
-<link href="../css/styles.css" type="text/css" rel="stylesheet">
+<title><?php bloginfo('description'); ?> | <?php bloginfo('name'); ?></title>
+    
+    <meta name="viewport" content="user-scalable=no, initial-scale=1.0, maximum-scale=1.0" />
+    
+    <!--begin styles-->
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="all" /> 
+<!--end styles-->
+    
 </head>
 
-<body>
+<body <?php body_class(); ?>>
 
 <nav>
 <a href="adamkozie.com"><img src="../images/ak-logo-icon.png" alt="AK logo"></a>
@@ -20,53 +27,10 @@
 
 <main>
 
-<div class="textimage">
-<img src="../images/ak-logo.jpg" alt="AK logo">
-<h1>Adam Kozie</h1>
-<hr>
-<h2>Web Design</h2>
-</div><!--end textimage-->
-
-<div class="text">
-<p>I am a web designer and a student in the Seattle Central web design and development program. This place is a simple staging area for my websites and exercises. Welcome.</p>
-<hr class="intro">
-</div><!--end text-->
-
-<div class="banner">
-<a target="_blank" href="exex/index.php"><img src="../images/banner-exex.jpg" alt="ExEx banner" /></a>
-</div><!--end banner-->
-<div class="text">
-<p>This site was the final project for a web publishing class.</p>
-<ul>
-<li><a target="_blank" href="https://docs.google.com/document/d/12F4aKW18WFsp4hQ9mXCob0FiLhEkIQ1jOncpRH9sJ50/edit?usp=sharing">ExEx Metadata</a></li>
-<li><a target="_blank" href="https://docs.google.com/document/d/1-Av-YD7huhDYcDL6gokdC49L4S4CZWTid6qmwZgiMIw/edit?usp=sharing">ExEx Wireframe</a></li>
-<li><a target="_blank" href="https://docs.google.com/document/d/1Eg-c_FCCzhNPyd8ZRYj6_bIy1BpXrFE1HIJUEPiy1sc/edit?usp=sharing">ExEx Communication Brief</a></li>
-<li><a target="_blank" href="https://docs.google.com/document/d/1fJ3mf6OTat0AkLeAjgYGiUFGCazlzt20UPcLXlxsams/edit?usp=sharing">ExEx User Persona</a></li>
-<li><a target="_blank" href="https://docs.google.com/document/d/1ySCN4QVzoDbamqovalE6DqhcHMe4PxNTROPGaXumDS8/edit?usp=sharing">ExEx Maintenance Survey</a></li>
-</ul>
-</div><!--end text-->
-
-<div class="banner">
-<a target="_blank" href="http://www.northernthorns.com"><img src="../images/banner-northern-thorns.jpg" alt="Northern Thorns banner" /></a>
-</div><!--end banner-->
-<div class="text">
-<p>A band website I made for the group Northern Thorns. Adapted from the same CSS as the ExEx site.</p>
-</div><!--end text-->
-
-<div class="banner">
-<a target="_blank" href="afropop/index.php"><img src="../images/banner-afropop.jpg" alt="Afropop banner" /></a>
-</div><!--end banner-->
-<div class="text">
-<p>The Afropop site was an exercise for a class, my first site with dynamic navigation and PHP includes.</p>
-</div><!--end text-->
-
-<div class="banner">
-<a target="_blank" href="lasara/index.html"><img src="../images/banner-lasara.jpg" alt="Lasara banner" /></a>
-</div><!--end banner-->
-<div class="text">
-<p>Essentially the first site I made, for a yoga and fitness instructor. This was the final for my first web publishing class.</p>
-</div><!--end text-->
-
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); // start the loop ?>
+<h2><a href="<?php the_permalink(); // link to the page or posting ?>"><?php the_title(); // get the page or posting title ?></a></h2>
+<?php the_content(''); // get page or posting written content ?>
+<?php endwhile; endif; // end the loop ?>
 </main>
 
 <footer>
@@ -80,6 +44,6 @@
 
 
 
-
+<?php wp_footer(); ?>
 </body>
 </html>
